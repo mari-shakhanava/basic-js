0265463b0160f78b7c626656070c0da8c6359985
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-01 02:02:06 +0300

    feat: finish cats finder algorithm

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catsNumber = 0;
+    for(let i = 0; i < matrix.length; i++) {
+        for(let n = 0; n < matrix[i].length; n++) {
+            if (matrix[i][n] === '^^'){
+              catsNumber++;
+            }
+        }
+
+    } return catsNumber;
 };
