9bf6f5abacd14891a1732997b3dd3b3d27cf4e64
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-09 23:51:52 +0300

    feat: add part of algorithm for dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam( members) {
+    if ( !members || Array.isArray(members) === false){
+    return false;
+  } else {
+      membersCorrect = members.map( function(x) {
+          if (typeof x === 'string') {
+              return x;
+          }});
+      membersCorrectOnlyLettersSorted = membersCorrect.map( function (t) {
+        return t.split(' ').sort().join('');
+      });
+      dreamTeamNameArr = membersCorrectOnlyLettersSorted.map( function (y) {
+        return y.toUpperCase().slice(0,1);
+      });
+      return dreamTeamName = dreamTeamNameArr.join('');
+
+  }
+
+    };
+
+
+
+
