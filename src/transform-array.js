52079fb5e9099d142d8f0d0db78376cdf80d380f
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-29 23:47:53 +0300

    feat: add algorithm for transform arr task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform( arr) {
+    if (Object.prototype.toString.call(arr) !== '[object Array]' || arr === null ) {
+        throw new Error();
+    }
+        let  newArr = arr.slice(0);
+        for (let i=0; i < newArr.length; i++){
+            if (newArr[i] === '--discard-next'){
+                newArr[i] = undefined;
+                newArr[i+1] = undefined;
+       } else if (newArr[i] === '--discard-prev'){
+                newArr[i] = undefined;
+                newArr[i-1] = undefined;
+       } else  if (newArr[i] === '--double-next'){
+         if (newArr[i+1] !== undefined){
+          newArr[i] = newArr[i+1];
+        } else newArr[i] = undefined;
+      } else  if (newArr[i] === '--double-prev') {
+                if (newArr[i-1] !== undefined){
+                    newArr[i] = newArr[i-1];
+                } else newArr[i] = undefined;
+       }
+        }
+    return newArr.filter(function (e) {
+        return e !== undefined;
+    });
+    };
+
+
