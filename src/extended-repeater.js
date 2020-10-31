2fcfaeb44f18a7c434a4c348f148018a37e3ba95
mari-shakhanava <maria.shakhanava@gmail.com>
2020-10-31 23:11:05 +0300

    feat: add algorithm for extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  // throw new CustomError('Not implemented');
+  let defOptions = {
+      repeatTimes: 1,
+      separator: '+',
+      addition: '',
+      additionRepeatTimes: 1,
+      additionSeparator: '|'
+  };
+
+  let separatorString = '';
+  let additionStr = String(options.addition);
+
+   if (!options.separator){
+     options.separator = defOptions.separator;
+   }
+   if (!options.additionSeparator){
+     options.additionSeparator = defOptions.additionSeparator;
+   }
+
+   if (!options.repeatTimes){
+     options.repeatTimes = defOptions.repeatTimes;
+   }
+    if (!options.additionRepeatTimes){
+        options.additionRepeatTimes = defOptions.additionRepeatTimes;
+    }
+
+    if (options.addition !== undefined){
+      separatorString = (additionStr + options.additionSeparator).repeat(options.additionRepeatTimes-1) + options.addition;
+    }
+
+    return resultStr = (str + separatorString + options.separator).repeat(options.repeatTimes-1)+ (str + separatorString);
+
 };
   
